fce5d29fcdc367dd8368181e4bd9e59841bab38d
Moshiur Rahman <mrahman9@upei.ca>
2021-07-18 19:56:39 -0300

    client side connected with backend with fetch,few styles added

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,10 +1,27 @@
 console.log("Client side javascript is loaded");
 
-fetch("http://localhost:3000/weather?address=badda").then(response => {
-	response.json().then(data => {
-		if (data.error) console.log(data.error);
-		else {
-			console.log(data);
-		}
+//fetching the data from the backend
+
+const weatherForm = document.querySelector("form");
+const textField = document.querySelector("input");
+const p1 = document.querySelector("#p1");
+const p2 = document.querySelector("#p2");
+
+weatherForm.addEventListener("submit", e => {
+	e.preventDefault();
+	const loca = textField.value;
+
+	p1.textContent = "Loading...";
+	p2.textContent = "";
+
+	fetch("http://localhost:3000/weather?address=" + loca).then(response => {
+		response.json().then(data => {
+			if (data.error) p1.textContent = data.error;
+			else {
+				p1.textContent = data.location;
+				p2.textContent =
+					data.weather + "  " + data.temperature + "  " + data.feels_like;
+			}
+		});
 	});
 });
