a93a660e59bdd11ce6c61cf601262d0d218facab
Moshiur Rahman <mrahman9@upei.ca>
2021-07-18 19:08:30 -0300

    section 7 done, 8 half done, css added

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,10 @@
-console.log("Client side javascript is loaded")
+console.log("Client side javascript is loaded");
+
+fetch("http://localhost:3000/weather?address=badda").then(response => {
+	response.json().then(data => {
+		if (data.error) console.log(data.error);
+		else {
+			console.log(data);
+		}
+	});
+});
